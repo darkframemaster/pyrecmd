15f12c3219308ef30fb45510a8066510a6d12b29
darkframexue <darkframexue@outlook.com>
2017-08-30 12:32:56 +0800

    init repo

---
+++ als.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env
+
+import numpy
+
+def als(R, K, steps = 15, beta = 0.0002):
+	M, N = R.shape
+	X = numpy.random.rand(M, K)
+	X = numpy.mat(X)
+	Y = numpy.random.rand(N, K)
+	Y = numpy.mat(Y)
+	I = numpy.mat(numpy.eye(K))
+
+	err = list()
+	it  = list()
+	for step in xrange(steps):
+		X1 = list()
+		Y1 = list()
+
+		P = (Y.T * Y + beta * I).I * Y.T
+
+		for i in xrange(M):
+			X1.append([j[0,0] for j in (P * R[i, :].T)])
+		X = numpy.mat(X1)
+
+		Q = (X.T * X + beta * I).I * X.T
+		for i in xrange(N):
+			Y1.append([j[0,0] for j in (Q * R[:, i])])
+		Y = numpy.mat(Y1)
+
+		it.append(step)
+		err.append(numpy.sqrt(numpy.sum(pow(numpy.array(R - X * Y.T), 2)) / (M * N)))
+	return it, err
+
+if __name__ == "__main__":
+	M = 100
+    F = 30
+    U = 300
+    R = numpy.matrix(numpy.random.rand(M, F)) * numpy.matrix(numpy.random.rand(U, F)).T
+
+    it, err1 = als(R, F, beta=0.1)
+    it, err2 = als(R, F, beta=0.01)
+    it, err3 = als(R, F, beta=0.001)
+    it, err4 = als(R, F, beta=0.0001)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(it, err1, 'r', label='beta=0.1')
+    ax.plot(it, err2, 'b', label='beta=0.01')
+    ax.plot(it, err3, 'y', label='beta=0.001')
+    ax.plot(it, err4, 'g', label='beta=0.0001')
+    ax.legend(loc='upper right')
+    plt.xlabel('iterations')
+    plt.ylabel('rsme')
+    plt.show()
---
+++ recommend.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python
+#-*- coding:utf-8 -*-
+from math import sqrt
+
+def sim_distance(prefs, obj1, obj2):
+    # Euclidation Distance Score
+    # compute the Eucildation Distance Score between two element the higher the better
+    si = dict()
+    for item in prefs[obj1]:
+        if item in prefs[obj2]:
+            si[item] = 1
+
+    if len(si) == 0:
+        return 0
+
+    sum_of_squares = sum([
+        pow(prefs[obj1][item]-prefs[obj2][item], 2)
+        for item in prefs[obj1] if item in prefs[obj2]
+        ])
+    return 1 / (1 + sqrt(sum_of_squares))
+
+def sim_pearson(prefs, obj1, obj2):
+    # Pearson Correlation Score
+    # More complex than Euclidation Distance Score, but work better when data set is less normalized
+    # ex: when a user's score is always lower than the average value.
+    # the higher the better
+    si = dict()
+    for item in prefs[obj1]:
+        if item in prefs[obj2]:
+            si[item] = 1
+
+    n = len(si)
+    if n == 0:
+        return 0
+
+    sum1 = sum([prefs[obj1][item] for item in si])
+    sum2 = sum([prefs[obj2][item] for item in si])
+
+    sum1Sq = sum([pow(prefs[obj1][item], 2) for item in si])
+    sum2Sq = sum([pow(prefs[obj2][item], 2) for item in si])
+
+    pSum = sum([prefs[obj1][item] * prefs[obj2][item] for item in si])
+
+    num = pSum - (sum1 * sum2 / n)
+    den = sqrt((sum1Sq - pow(sum1, 2) / n) * (sum2Sq - pow(sum2, 2) / n))
+    if den == 0:
+        return 0
+    return num / den
+
+def topMatches(prefs, obj, n = 5, similarity = sim_pearson):
+    # find the most similarity obj of the giving one.
+    scores = [(similarity(prefs, obj, other), other) 
+                for other in prefs if other != obj]
+
+    scores.sort()
+    scores.reverse()
+    return scores[:n]
+
+def get_recommendations(prefs, obj, similarity = sim_pearson):
+    # making recommendations for giving obj
+
+    totals = dict()
+    sim_sums = dict()
+
+    for other in prefs:
+        if other != obj
+            sim = similarity(prefs, obj, other)
+        else:
+            continue
+
+        if sim <= 0:
+            continue
+        for item in prefs[other]:
+            if item not in prefs[obj] or prefs[obj][item] == 0:
+                totals.setdefault(item, 0)
+                totals[item] += prefs[other][item] * sim
+                sim_sums.setdefault(item, 0)
+                sim_sums[item] += sim
+
+    rankings = [(totals / sim_sums[item], item) for item, total in totals.items()]
+
+    rankings.sort()
+    rankings.reverse()
+    return rankings
+
+def transfrom_prefs(prefs):
+    result = dict()
+    for obj in result:
+        for item in prefs[obj]:
+            result.setdefault(item, dict())
+            result[item][obj] = prefs[obj][item]
+    return result
